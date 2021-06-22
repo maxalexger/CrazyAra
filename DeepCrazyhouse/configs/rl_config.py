76b73e261ba33496175a157189d026c293cbeec3
maxalexger <saaQQMBFCY8Putb>
2021-06-22 21:30:18 +0200

    MultiAra Changes needed to start training/RL

--- DeepCrazyhouse/configs/rl_config.py
+++ DeepCrazyhouse/configs/rl_config.py
@@ -12,22 +12,18 @@ from dataclasses import dataclass
 @dataclass
 class RLConfig:
     """Dataclass storing the options (except UCI options) for executing reinforcement learning."""
-    # How many arena games will be done to judge the quality of the new network
     arena_games: int = 100
-    # Directory where the executable is located and where the selfplay data will be stored
     binary_dir: str = f'/data/RL/'
     binary_name: str = f'MultiAra'
-    # How many times to train the NN, create a model contender or generate nn_update_files games
     nb_nn_updates: int = 10
-    # How many new generated training files are needed to apply an update to the NN
     nn_update_files: int = 10
     precision: str = f'float16'
+
     # Replay Memory
-    rm_nb_files: int = 5  # how many data packages/files shall be randomly taken from memory
-    rm_fraction_for_selection: float = 0.05  # which percentage of the most recent memory shall be taken into account
-    # The UCI_Variant. Must be in ["3check", "atomic", "chess", "crazyhouse",
-    # "giveaway" (= antichess), "horde", "kingofthehill", "racingkings"]
-    uci_variant: str = f'atomic'
+    rm_nb_files: int = 5
+    rm_fraction_for_selection: float = 0.05
+
+    uci_variant: str = f'3check'
 
 
 @dataclass
@@ -38,28 +34,54 @@ class UCIConfig:
     """
     Allow_Early_Stopping: bool = False
     Batch_Size: int = 8
-    Centi_Dirichlet_Alpha: int = 30  # default: 20
+
+    Centi_CPuct_Init: int = 250
+    Centi_Dirichlet_Alpha: int = 20
     Centi_Dirichlet_Epsilon: int = 25
     Centi_Epsilon_Checks: int = 0
     Centi_Epsilon_Greedy: int = 0
     Centi_Node_Temperature: int = 100
-    Centi_Resign_Probability: int = 90
-    Centi_Q_Value_Weight: int = 0
+    Centi_Q_Value_Weight: int = 100
+    Centi_Q_Veto_Delta: int = 40
+
+    Centi_Quick_Dirichlet_Epsilon: int = 0
     Centi_Quick_Probability: int = 0
-    Centi_Temperature: int = 80
-    MaxInitPly: int = 0  # default: 30
-    MCTS_Solver: bool = False
-    MeanInitPly: int = 0  # default: 15
-    Milli_Policy_Clip_Thresh: int = 10
+    Centi_Quick_Q_Value_Weight: int = 0
+    Quick_Nodes: int = 0
+
+    Centi_Random_Move_Factor: int = 0
+    Centi_Resign_Probability: int = 0
+    Centi_Resign_Threshold: int = 0
+    Centi_U_Init_Divisor: int = 100
+
+    Fixed_Movetime: int = 1000
+    Centi_Virtual_Loss: int = 100
+    MCTS_Solver: bool = True
+    Milli_Policy_Clip_Thresh: int = 0
+    Move_Overhead: int = 0
+
     Nodes: int = 800
+    Centi_Node_Random_Factor: int = 10
+
     Reuse_Tree: str = False
-    Search_Type: str = f'mcts'
-    Selfplay_Chunk_Size: int = 128  # default: 128
-    Selfplay_Number_Chunks: int = 640  # default: 640
+    Search_Type: str = f'mcgs'
+    Selfplay_Chunk_Size: int = 128
+    Selfplay_Number_Chunks: int = 640
     Simulations: int = 3200
     SyzygyPath: str = f''
-    Temperature_Moves: int = 15  # CZ: 500
     Timeout_MS: int = 0
+    Use_NPS_Time_Manager: bool = False
+
+    # Initial book moves
+    MaxInitPly: int = 30
+    MeanInitPly: int = 8
+    Centi_Raw_Prob_Temperature: int = 5
+
+    # Initial temperature moves (after init book moves)
+    Centi_Temperature: int = 80
+    Centi_Temperature_Decay: int = 92
+    Centi_Quantile_Clipping: int = 0
+    Temperature_Moves: int = 15
 
 
 @dataclass
