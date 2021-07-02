78a4cae9624e0ca9e9b33d769a3d8dd0ba0bd94a
maxalexger <saaQQMBFCY8Putb>
2021-07-02 21:46:35 +0200

    3check 40 sl

--- tournaments/runtournament.py
+++ tournaments/runtournament.py
@@ -56,18 +56,24 @@ setup = {
         ['crazyhouse', [ARA_ENGINE_UP40, ARA_ENGINE_UP50], FAST_MODE],
         ['crazyhouse', [ARA_ENGINE_UP40, ARA_ENGINE_UP50], LONG_MODE],
     ],
-    5: [
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_UP30], FAST_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_UP30], LONG_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, FAIRY_ENGINE], FAST_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, FAIRY_ENGINE], LONG_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_SL7], FAST_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_SL7], LONG_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP20_FROM0, ARA_ENGINE_UP30_FROM0], FAST_MODE],
-        ['kingofthehill', [ARA_ENGINE_UP20_FROM0, ARA_ENGINE_UP30_FROM0], LONG_MODE],
-    ],
+    # 5: [
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_UP30], FAST_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_UP30], LONG_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, FAIRY_ENGINE], FAST_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, FAIRY_ENGINE], LONG_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_SL7], FAST_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP30_FROM0, ARA_ENGINE_SL7], LONG_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP20_FROM0, ARA_ENGINE_UP30_FROM0], FAST_MODE],
+    #     ['kingofthehill', [ARA_ENGINE_UP20_FROM0, ARA_ENGINE_UP30_FROM0], LONG_MODE],
+    # ],
     4: [
         ['antichess', [ARA_ENGINE_UP50_STATIC_TC_LONG, FAIRY_ENGINE], FAST_MODE]
+    ],
+    5: [
+        ['3check', [ARA_ENGINE_UP40, FAIRY_ENGINE], FAST_MODE],
+        ['3check', [ARA_ENGINE_UP40, FAIRY_ENGINE], LONG_MODE],
+        ['3check', [ARA_ENGINE_UP30, ARA_ENGINE_UP40], FAST_MODE],
+        ['3check', [ARA_ENGINE_UP30, ARA_ENGINE_UP40], LONG_MODE],
     ]
 }
 
