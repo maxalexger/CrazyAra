101af2eb9308cfff723f24f4b50e0bb3b592f47b
maxalexger <saaQQMBFCY8Putb>
2021-07-08 11:21:37 +0200

    do not print out info apply move to tree

--- engine/src/agents/mctsagent.cpp
+++ engine/src/agents/mctsagent.cpp
@@ -223,12 +223,12 @@ void MCTSAgent::apply_move_to_tree(Action move, bool ownMove)
 {
     if (!reusedFullTree && rootNode != nullptr && rootNode->is_playout_node()) {
         if (ownMove) {
-            info_string("apply move to tree");
+            // info_string("apply move to tree");
             opponentsNextRoot = pick_next_node(move, rootNode.get());
             return;
         }
         else if (opponentsNextRoot != nullptr && opponentsNextRoot->is_playout_node()){
-            info_string("apply move to tree");
+            // info_string("apply move to tree");
             ownNextRoot = pick_next_node(move, opponentsNextRoot.get());
             return;
         }
